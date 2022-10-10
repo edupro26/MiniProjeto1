8d6b010786dce989a9f413cadb647efcc9e718aa
Eduardo Proença <edupro26@gmail.com>
2022-10-10 16:43:35 +0100

    Metodo MyHttpClient

--- MiniProjeto1/src/com/miniprojeto1/httpserver/MyHttpClient.java
+++ MiniProjeto1/src/com/miniprojeto1/httpserver/MyHttpClient.java
@@ -1,6 +1,37 @@
 package com.miniprojeto1.httpserver;
 
+import java.io.IOException;
+
 public class MyHttpClient {
+
+	public MyHttpClient(String hostName, int portNumber) throws IOException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void getResource(String ObjectName) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void postData(String[] data) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sendUnimplementedMethod(String wrongMethodName) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void malformedRequest(int type) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
