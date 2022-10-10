a2fdeb786992c1354eaeeac2c339e2dd08066e97
Eduardo Proença <edupro26@gmail.com>
2022-10-10 15:38:26 +0100

    Commit teste

--- MiniProjeto1/src/com/miniprojeto1/httpserver/MyHttpServer.java
+++ MiniProjeto1/src/com/miniprojeto1/httpserver/MyHttpServer.java
@@ -3,8 +3,9 @@ package com.miniprojeto1.httpserver;
 public class MyHttpServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Server starting...");
+		
 	}
 
 }
