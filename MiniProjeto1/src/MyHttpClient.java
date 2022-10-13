32c3054a0de85e16d27d0655b9cdd7920c760210
Eduardo Proença <edupro26@gmail.com>
2022-10-14 00:49:49 +0100

    implementação do get index.html imcompleto

--- MiniProjeto1/src/MyHttpClient.java
+++ MiniProjeto1/src/MyHttpClient.java
@@ -1,17 +1,32 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MyHttpClient {
 
 	private String hostName;
 	private int portNumber;
+	private Socket socket;
 	
 	public MyHttpClient(String hostName, int portNumber) throws IOException {
-		// TODO Auto-generated constructor stub
+		this.hostName = hostName;
+		this.portNumber = portNumber;
+		
+		this.socket = new Socket(hostName, portNumber);
 	}
 
 	public void getResource(String ObjectName) throws IOException {
-		// TODO Auto-generated method stub
-		
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+        String request = 
+        		"GET /" + ObjectName + " HTTP/1.1" + "\r\n" +
+        		"Host: " + hostName + ":" + portNumber + "\r\n";
+        out.println(request);
+        
+        System.out.println("\r\n" + in.readLine() + "\r\n");
 	}
 
 	public void postData(String[] data) throws IOException {
--- MiniProjeto1/src/MyHttpServer.java
+++ MiniProjeto1/src/MyHttpServer.java
@@ -1,11 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MyHttpServer {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Server started");
-		
-		// TODO
+	
+	private static String getRequestObj(String ObjectName) throws FileNotFoundException {
+		StringBuilder content = new StringBuilder();
+        FileReader fReader = new FileReader(ObjectName);
+        String result = null;
+        try {
+            BufferedReader bReader = new BufferedReader(fReader);
+            String value;
+            while ((value = bReader.readLine()) != null) {
+                content.append(value);
+            }
+            result = content.toString();
+            bReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return result;
 	}
-
+	
+	public static void main(String[] args) throws IOException {
+		
+		if(args.length != 1) {
+			System.err.println("Usage: java EchoServer <port number>");
+			System.exit(1);
+		}
+		
+		int portNumber = Integer.parseInt(args[0]);
+		try (ServerSocket server = new ServerSocket(portNumber)) {
+			System.out.println("Listening for request on port: " + portNumber + " ..." + "\r\n");
+			while (true) {
+				Socket socket = server.accept();
+				System.out.println("Client connect.\r\n");		
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String request;
+				while ((request = in.readLine()) != null) {
+					System.out.println(request);
+					
+					//TODO imprimir conteudo do index.html
+					
+					String answer = "HTTP/1.1 200 OK\r\n";
+					out.println(answer);
+				}
+			}
+		} catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+		
+	}
+	
 }
