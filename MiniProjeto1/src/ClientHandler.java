e24d10377e7bd0cb0705f50761a10aec66e3cd8a
Eduardo Proença <edupro26@gmail.com>
2022-10-14 18:33:33 +0100

    Added ClientHandler.java

---
+++ MiniProjeto1/src/ClientHandler.java
@@ -0,0 +1,19 @@
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+	
+	private Socket socket;
+
+	public ClientHandler(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		// implementar o metodo para correr as threads
+		// Alinea f.
+		
+	}
+	
+}
